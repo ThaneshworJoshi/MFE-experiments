bd82336c167d29e3422f20ca96537a3c400e67b8
thaneshwor <thaneshwor10@gmail.com>
2025-01-01 12:08:50 +0545

    feat: Project cleanup

--- container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as productMount} from 'products/ProductsIndex'; // Import the ProductsIndex from the products project
-import { mount as cartMount } from 'cart/CartShow'; // Import the CartShow from the cart project
-
-productMount(document.querySelector('#my-products')); // Mount the ProductsIndex component in the container application
-cartMount(document.querySelector('#my-cart'));
